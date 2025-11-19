339fffe83c9e333c9fd675b45fd7f7b713ba9026
Teorema <anyeloladino41@gmail.com>
2025-11-18 20:04:33 -0500

    .

--- unidad2/actividad3/motos_project/formulario_motos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FormularioMotosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'formulario_motos'
--- unidad2/actividad3/motos_project/formulario_motos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_motocicletas, name='lista_motocicletas'),
-    path('crear/', views.crear_motocicleta, name='crear_motocicleta'),
-    path('eliminar/<int:moto_id>/', views.eliminar_motocicleta, name='eliminar_motocicleta'),
-]
